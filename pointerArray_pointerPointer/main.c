e9e3731ba9226f5ebbba81bd10fd81d93834c47c
minhkhai <khaiminhphannguyen@gmail.com>
2024-06-10 21:00:02 +0700

    modifying main and qsort functions

--- pointerArray_pointerPointer/main.c
+++ pointerArray_pointerPointer/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "my_lib.h"
+#define MAXLINES 5000
 int main()
 {
-    char myName[100];
-    getline(myName, 100);
-    printf("ok\n");
-    printf("My name: %s\n",myName);
-    return 0;
+    char *lineptr[MAXLINES];
+    int nlines;
+    if((nlines = readlines(lineptr, MAXLINES)) >= 0){
+        myqsort(lineptr, 0, nlines -1);
+        writelines(lineptr, nlines);
+        return 0;
+    }else{
+        printf("error\n");
+        return 1;
+    }
 }
