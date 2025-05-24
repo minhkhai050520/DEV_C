9b8a09f5d3a39631c313c38b4f7e1336a1174ffd
unknown <khaiminhphannguyen@gmail.com>
2025-05-25 00:20:28 +0700

    queue

---
+++ stack_queue/queue.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int size;
+    int head;
+    int tail;
+    int *pt;
+} queue;
+
+void enqueue(queue *q, int x);
+int dequeue(queue *q);
+void initqueue(queue *q, int size);
+
+int main()
+{
+    queue Q;
+    int size;
+    char c;
+    printf("Enter a size of the queue: ");
+    scanf("%d", &size);
+    initqueue(&Q, size);
+
+    printf("i - insert\np - pop\n");
+    while((c = getchar()) != EOF)
+    {
+        switch(c)
+        {
+            case 'i':
+            {
+                int a;
+                printf("input element: ");
+                scanf("%d", &a);
+                enqueue(&Q, a);
+                printf("Q.head: %d\n", Q.head);
+                printf("Q.tail: %d\n", Q.tail);
+                printf("Q.size: %d\n", Q.size);
+                break;
+            }
+            case 'p':
+                printf("dequeue: %d\n", dequeue(&Q));
+                printf("Q.head: %d\n", Q.head);
+                printf("Q.tail: %d\n", Q.tail);
+                printf("Q.size: %d\n", Q.size);
+                break;
+            default:
+                break;
+        }
+
+        if(c == 'q')
+            break;
+    }
+
+    
+    free(Q.pt);
+    return 0;
+}
+
+void initqueue(queue *q, int size)
+{
+    q->pt = malloc(size * sizeof(int));
+    q->size = size;
+    q->head = 0;
+    q->tail = 0;
+}
+
+void enqueue(queue *q, int x)
+{
+    if((q->tail + 1) == q->head || (q->head == 0 && q->tail == (q->size -1)))
+    {
+        printf("Queue is full\n");
+        return;
+    }
+
+    q->pt[q->tail] = x;
+
+    if(q->tail == (q->size - 1))
+        q->tail = 0;
+    else
+        q->tail += 1;
+}
+
+int dequeue(queue *q)
+{
+    int x;
+    if(q->head == q->tail)
+    {
+        printf("Queue is empty\n");
+        return -1;
+    }
+    x = q->pt[q->head];
+
+    if(q->head == (q->size -1))
+        q->head = 0;
+    else
+        q->head += 1;
+    return x;
+}
